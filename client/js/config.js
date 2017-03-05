1dcfac6cce060f8a4768f7dcf245c1e8d8e758e5
Raúl Yeguas <neokore@gmail.com>
2017-03-05 13:51:35 +0100

    Remove config.js file

--- client/js/config.js
+++
@@ -1,4 +0,0 @@
-var config = {
-  wssHost: 'wss://192.168.1.41', // Put here the IP of the host
-  screenSharingMode: 'screen' // It could be 'screen', 'application' or 'window' (currently only on Firefox)
-};
